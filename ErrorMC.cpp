d39c506df8dbe9d444ae140629c1911bd4eb637c
Paulo V.R.G. Silva <pvrecchia@gmail.com>
2020-03-11 14:43:27 -0300

    Add Error calc function

--- ErrorMC.cpp
+++ ErrorMC.cpp
@@ -36,11 +36,11 @@
 #include<vector>
 #include<cmath>
 
-//GSL Libraries
-#include<gsl/gsl_rng.h> // GSL Random number generation
-#include<gsl/gsl_randist.h>  // GSL Random distributions
+// //GSL Libraries
+// #include<gsl/gsl_rng.h> // GSL Random number generation
+// #include<gsl/gsl_randist.h>  // GSL Random distributions
 
-#include<ErrorMC.h> //header file
+// #include "ErrorMC.h" //header file
 
 //==============================================================================
 // Function Implementations
@@ -51,20 +51,20 @@
 //Inputs: vectors with parameters central values and uncertainties,
 // pointer to random number generator (GSL) and vector to store result
 int GenParMC
-(vector<double> par_central, vector<double> par_sigma,
-gsl_rng *r ,vector<double> &result)
-}
+(std::vector<double> par_central, std::vector<double> par_sigma,
+ gsl_rng *r ,std::vector<double> &result)
+{
   int npar = par_central.size();
   int npar_sigma = par_sigma.size();
 
   if(npar != npar_sigma){
-    cout << "Parameters and Sigma must have same number of entries.\n";
+    std::cout << "Parameters and Sigma must have same number of entries.\n";
     return -1;
   }
 
 
   for(int i=0; i<npar;i++){
-    result.push_back(gsl_ran_gaussian(rand,par_sigma.at(i)) + par_central.at(i));
+    result.push_back(gsl_ran_gaussian(r,par_sigma.at(i)) + par_central.at(i));
   }
 
   return 0;
@@ -73,4 +73,79 @@ gsl_rng *r ,vector<double> &result)
 //end of function GenParMC
 
 //------------------------------------------------------------------------------
-//Error Propagation
+//Error Propagation Calculation
+//Inputs: independent variable of the model (x)
+//vectors with parameters central values and uncertainties,
+//function with model and extra parameters (both defined by user),
+//maximum number of points to be calculated (user's choice).
+//It creats random number generator and performs MC to calculate
+//Returns: error, and mean value (by reference)
+double ErrorCalc
+(double x, std::vector<double> par_central, std::vector<double> par_sigma,
+ double(*model)(double,std::vector<double>,void*), void *extra_par,
+ int Nmax, double &average)
+{
+
+  //creating random number generator
+  //(for now, using default parameters)
+
+  unsigned long int seed = 0; //seed
+
+  const gsl_rng_type *T = gsl_rng_default;
+  gsl_rng *r = gsl_rng_alloc(T);
+  gsl_rng_set(r,seed);
+
+  std::vector<double> Ymc, parmc;
+  double y; int status;
+
+// loop for calculations
+  for(int i=0;i<Nmax;++i){
+    status = GenParMC(par_central,par_sigma,r,parmc);
+    if(status==-1) continue; //-1 means error, and should skip lines below
+    y = model(x,parmc,extra_par);//
+    Ymc.push_back(y);
+    parmc.clear();
+  }
+
+  //mean value and standard deviation
+
+  double sdev = 0;
+  double sum1 = 0;
+  double sum2 = 0;
+
+  int Ndata = Ymc.size();
+
+//mean
+
+  for(int i=0;i<Ndata;++i){
+    y = Ymc.at(i);
+    sum1 = sum1+y;
+    // sum2 = sum2 + y*y;
+  }
+
+  average = sum1/Ndata;
+
+  // const double *aux = &Ymc[0];
+  // average = gsl_stats_mean(aux,1,Ndata);
+
+// standard deviation
+
+
+  double Delta = 0;//abs(sum2 - sum1*sum1);
+
+  for(int i=0;i<Ndata;++i){
+    Delta = Ymc.at(i)-average;
+    sum2 = sum2 + Delta*Delta;
+  }
+
+  sdev = sqrt(sum2/(Ndata-1.0)); // problem?
+
+  // sdev = gsl_stats_sd_m(aux,1,Ndata,average);
+
+  gsl_rng_free(r); // free memory associated to random number generator
+  Ymc.clear();
+  parmc.clear();
+
+  return sdev;
+}
+//end of function ErrorCalc
