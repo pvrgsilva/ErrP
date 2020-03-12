e031904bfe519e4c72655f4c11a14a62358a5366
Paulo V.R.G. Silva <pvrecchia@gmail.com>
2020-03-12 18:21:00 -0300

    Implement and modify several functions
    Implementations and modifications of several functions
    as constructor, destructor, functions to set parameters.
    Tested and working ok.

--- ErrorMC.h
+++ ErrorMC.h
@@ -50,7 +50,7 @@
 //#include "ErrorMCdef.h" //file with functions implementations
 
 
-//typedef double (*funcmodel_t) (double,std::vector<double>,void*);
+typedef double (*funcmodel_t) (double,std::vector<double>,void*);
 
 //#include "ErrorMC.cpp" //file with functions implementations
 
@@ -62,23 +62,28 @@ class ErrorMC {
 // private:
   std::vector<double> par_central;
   std::vector<double> par_sigma;
-  std::vector<double> result;
+  // std::vector<double> result;
   gsl_rng *r;
-  int Nmax;
-  double average;
   void *extra_par;
+  funcmodel_t model;
+  int Nmax;
 
 public:
+  double average;
 
   ErrorMC(); //constructor (create others?)
+  ~ErrorMC(); //destructor
   void SetModel();
+  void SetN(int);
   void SetParCentral(std::vector<double>);
   void SetParSigma(std::vector<double>);
   // void SetParCov(); //to be implemented
   void SetExtraPar(void *);
+  void SetModel(funcmodel_t);
 
 //Parameter Generator
-  int GenParMC(std::vector<double>,std::vector<double>,gsl_rng*,std::vector<double>&);
+  // int GenParMC(std::vector<double>,std::vector<double>,gsl_rng*,std::vector<double>&);
+  int GenParMC(std::vector<double>&);
 
   //Error Propagation Calculation
   //Inputs: independent variable of the model (x)
@@ -88,11 +93,15 @@ public:
   //It creats random number generator and performs MC to calculate
   //Returns: error, and mean value (by reference)
     double ErrorCalc
-    (double,std::vector<double>,std::vector<double>,
-    double(*)(double,std::vector<double>,void*),
-    //funcmodel_t,
-    void*,int,double&);
-
+    // (double,std::vector<double>,std::vector<double>,
+    // double(*)(double,std::vector<double>,void*),
+    // //funcmodel_t,
+    // void*,int,double&);
+    (double,
+     // double(*)(double,std::vector<double>,void*),
+     // funcmodel_t,
+     // int,
+     double&);
 };
 
 //=============================================================================
