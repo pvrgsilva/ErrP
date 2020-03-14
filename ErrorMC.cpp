5c070386250ca116676e5462a3fb20955a9f5679
Paulo V.R.G. Silva <pvrecchia@gmail.com>
2020-03-14 18:05:01 -0300

    Add functions to propagate w/ cov
    Addition of functions to propagate with covariance
    Still needs further testes and look for possible mistakes

--- ErrorMC.cpp
+++ ErrorMC.cpp
@@ -132,6 +132,9 @@ void ErrorMC::SetParCov(gsl_matrix *user_matrix){
   }else{
     covmatrix = gsl_matrix_alloc(Npar,Npar);
     gsl_matrix_memcpy(covmatrix, user_matrix);
+
+    //perform Cholesky decomposition (needed for error propagation)
+    gsl_linalg_cholesky_decomp1(covmatrix);
   }
 
 }
@@ -150,7 +153,14 @@ void ErrorMC::SetParCov(const char *file_path){
       std::cout << "\nMatrix elements read with success from file.\n";
     }
     fclose(mfile);
+
+    //perform Cholesky decomposition (needed for error propagation)
+    gsl_linalg_cholesky_decomp1(covmatrix);
   }
+
+
+
+
 }
 
 
@@ -219,16 +229,16 @@ int ErrorMC::GenParMC
 // std::vector<double> par_central, std::vector<double> par_sigma,gsl_rng *r ,
  std::vector<double> &result)
 {
-  int npar = par_central.size();
+  // int npar = par_central.size();
   int npar_sigma = par_sigma.size();
 
-  if(npar != npar_sigma){
+  if(Npar != npar_sigma){
     std::cout << "Parameters and Sigma must have same number of entries.\n";
     return -1;
   }
 
 
-  for(int i=0; i<npar;i++){
+  for(int i=0; i<Npar;i++){
     if(par_sigma.at(i)!=0){
       result.push_back(gsl_ran_gaussian(r,par_sigma.at(i)) + par_central.at(i));
     }else{
@@ -241,6 +251,52 @@ int ErrorMC::GenParMC
 
 }
 //end of function GenParMC
+
+
+
+//Parameter Generator with Covariance
+//Gaussian distribution
+//Inputs: vectors with parameters central values and uncertainties,
+// pointer to random numbn";
+  // std::cout << "Size: " << sizeof(extra_par) << "\n";
+  // std::cout << "Info " << *(float*)extra_par << "\n";
+  // std::cout << "Yes!!\n"er generator (GSL) and vector to store result
+
+int ErrorMC::GenParMCCov
+(
+// std::vector<double> par_central, std::vector<double> par_sigma,gsl_rng *r ,
+ std::vector<double> &result)
+{
+  // int npar = par_central.size();
+  int npar_sigma = par_sigma.size();
+
+  if(Npar != npar_sigma){
+    std::cout << "Parameters and Sigma must have same number of entries.\n";
+    return -1;
+  }
+
+
+  gsl_vector *res_aux = gsl_vector_alloc(Npar);
+  gsl_vector *mu = gsl_vector_alloc(Npar);
+
+  for(int i=0; i<Npar;i++){
+    gsl_vector_set(mu,i,par_central.at(i));
+  }
+
+  gsl_ran_multivariate_gaussian(r,mu,covmatrix,res_aux);
+
+  for(int i=0; i<Npar;i++){
+    result.push_back(gsl_vector_get(res_aux,i));
+  }
+
+  gsl_vector_free(res_aux);
+  gsl_vector_free(mu);
+
+  return 0;
+
+}
+//end of function GenParMCCov
+
 
 //------------------------------------------------------------------------------
 //Error Propagation Calculation
@@ -327,3 +383,92 @@ double ErrorMC::ErrorCalc
   return sdev;
 }
 //end of function ErrorCalc
+
+//------------------------------------------------------------------------------
+//Error Propagation Calculation with Covariance
+//****Working. Error still larger than from analytical calculations,
+// but still smaller than without covariance. Check method!!
+//
+//Inputs: independent variable of the model (x)
+//vectors with parameters central values and uncertainties,
+//function with model and extra parameters (both defined by user),
+//maximum number of points to be calculated (user's choice).
+//It creats random number generator and performs MC to calculate
+//Returns: error, and mean value (by reference)
+// double ErrorMC::ErrorCalc
+// (double x, std::vector<double> par_central, std::vector<double> par_sigma,
+//  double(*model)(double,std::vector<double>,void*),
+//  // funcmodel_t model,
+//  void *extra_par, int Nmax, double &average)
+double ErrorMC::ErrorCalcCov
+(double x,// std::vector<double> par_central, std::vector<double> par_sigma,
+ // double(*model)(double,std::vector<double>,void*),
+ // funcmodel_t model,
+ //void *extra_par,
+ // int Nmax,
+ double &average)
+{
+
+  // //creating random number generator
+  // //(for now, using default parameters)
+  //
+  // unsigned long int seed = 0; //seed
+  //
+  // const gsl_rng_type *T = gsl_rng_default;
+  // gsl_rng *r = gsl_rng_alloc(T);
+  // gsl_rng_set(r,seed);
+
+  std::vector<double> Ymc, parmc;
+  double y; int status;
+
+// loop for calculations
+  for(int i=0;i<Nmax;++i){
+    status = GenParMCCov(parmc);//(par_central,par_sigma,r,parmc);
+    if(status==-1) continue; //-1 means error, and should skip lines below
+    y = model(x,parmc,extra_par);//
+    Ymc.push_back(y);
+    parmc.clear();
+  }
+
+  //mean value and standard deviation
+
+  double sdev = 0;
+  double sum1 = 0;
+  double sum2 = 0;
+
+  int Ndata = Ymc.size();
+
+//mean
+
+  for(int i=0;i<Ndata;++i){
+    y = Ymc.at(i);
+    sum1 = sum1+y;
+    // sum2 = sum2 + y*y;
+  }
+
+  average = sum1/Ndata;
+
+  // const double *aux = &Ymc[0];
+  // average = gsl_stats_mean(aux,1,Ndata);
+
+// standard deviation
+
+
+  double Delta = 0;//abs(sum2 - sum1*sum1);
+
+  for(int i=0;i<Ndata;++i){
+    Delta = Ymc.at(i)-average;
+    sum2 = sum2 + Delta*Delta;
+  }
+
+  sdev = sqrt(sum2/(Ndata-1.0));
+
+  // sdev = gsl_stats_sd_m(aux,1,Ndata,average);
+
+  // gsl_rng_free(r); // free memory associated to random number generator
+  Ymc.clear();
+  parmc.clear();
+
+  return sdev;
+}
+//end of function ErrorCalcCov
